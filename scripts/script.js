243a2f5928d9c7dbf51ac0bbad7b1144eb40e0a3
Tony_T <antoninatimush@yandex.ru>
2023-03-09 21:24:13 +0300

    another fix

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const popupContainer = document.querySelector('.popup__container');
+const btnClosePopup = document.querySelector('.popup__close-button');
+const btnSaveChanges = document.querySelector('.popup__save-button');
+const popupInputName = document.querySelector('.popup__input_change_name');
+const popupInputAbout = document.querySelector('.popup__input_change_about');
+
+const btnEditProfile = document.querySelector('.profile__edit-button');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
