ef20af4b0d920af5c4f50483df770d5325c13cee
Tony_T <antoninatimush@yandex.ru>
2023-03-10 15:56:30 +0300

    first attempt

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,34 @@
-const popupContainer = document.querySelector('.popup__container');
-const btnClosePopup = document.querySelector('.popup__close-button');
-const btnSaveChanges = document.querySelector('.popup__save-button');
-const popupInputName = document.querySelector('.popup__input_change_name');
-const popupInputAbout = document.querySelector('.popup__input_change_about');
+//большая просьба ошибки по js расписывать максимально подробно, плаваю -_-
 
+const popupElement = document.querySelector('.popup');
 const btnEditProfile = document.querySelector('.profile__edit-button');
+const btnClosePopup = popupElement.querySelector('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
+const btnSaveChanges = popupElement.querySelector('.popup__save-button');
+
+btnEditProfile.addEventListener('click', () => {
+    popupElement.classList.add('popup_opened');
+});
+
+const closePopup = () => {
+    popupElement.classList.remove('popup_opened');
+}
+
+btnClosePopup.addEventListener('click', closePopup);
+
+const changeInfo = () => {
+    let popupInputName = popupElement.querySelector('.popup__input_change_name');
+    let popupInputAbout = popupElement.querySelector('.popup__input_change_about');
+    if (popupInputName.value.length === 0 || popupInputAbout.value.length === 0) {
+        closePopup();
+    } else {
+        profileName.innerText = popupInputName.value;
+        profileAbout.innerText = popupInputAbout.value;
+        popupInputName.value = '';
+        popupInputAbout.value = '';
+        closePopup();
+    }
+}
+
+btnSaveChanges.addEventListener('click', changeInfo);
